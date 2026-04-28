36cdcba2650315b5366c13cf34abd6ef9bd42a0a
Sifiso Cyprian Shezi <sifiso.cyprianshezi28@gmail.com>
2026-04-28 13:28:58 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# utility-ai-multi-tenant-learning-platform-cli
+
+Part of "ai-multi-tenant-learning-platform"
+Type: utility
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+const args = process.argv.slice(2);
+console.log("ai-multi-tenant-learning-platform-cli running with args:", args);
